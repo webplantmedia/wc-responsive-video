14f21b86db7dfc14382159e2c878b6f414270c3b
Chris Baldelomar <chris@webplantmedia.com>
2014-08-22 16:28:03 -0500

    Added responsive video support for jetpack

--- includes/functions.php
+++ includes/functions.php
@@ -16,6 +16,21 @@ if ( ! function_exists( 'wc_responsive_video_html' ) ) {
 }
 add_filter( 'embed_oembed_html', 'wc_responsive_video_html', 999, 4 );
 
+if ( ! function_exists( 'wc_responsive_video_html_jetpack' ) ) {
+	function wc_responsive_video_html_jetpack( $html ) {
+		if ( ! $ratio = wc_responsive_video_html_get_ratio( $html ) ) {
+			return $html;
+		}
+
+		$class = array();
+		$class[] = 'wc-responsive-video';
+		$class[] = 'wc-rv-ratio-' . $ratio;
+
+		return '<p class="' . implode( ' ', $class ) . '">' . $html . '</p>';
+	}
+}
+add_filter( 'video_embed_html', 'wc_responsive_video_html_jetpack', 999, 1 );
+
 if ( ! function_exists( 'wc_responsive_video_get_width_height' ) ) {
 	/**
 	 * Return the width and height value of html element
@@ -64,9 +79,13 @@ if ( ! function_exists( 'wc_responsive_video_get_ratio' ) ) {
 		$ratio = round( $width / $height, 1 );
 
 		switch ( $ratio ) {
+			case 1.7 :
 			case 1.8 :
+			case 1.9 :
 				return '16-9';
+			case 1.4 :
 			case 1.3 :
+			case 1.2 :
 				return '4-3';
 			default :
 				return 0;
